136605dfcad12927eb131b2173c2c46a036c7757
rayan17khan <119957434+rayan17khan@users.noreply.github.com>
2026-04-02 15:56:55 +0530

    fix cors

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI, UploadFile, File
 import shutil
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def detect_ai_image(path):
     return "✅ Likely Real Image"
